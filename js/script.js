e1847b438b2f4b5d034b91520a00c996c246167f
Andrey Zhulanov <Andrey.Zhulanov@gmail.com>
2021-01-12 21:14:00 +0500

    Issue 1 complete

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 'use strict'; // Испольуем нотации ES6
 
+// Задача 1
+
+let myName = 'Andrey';
+const age = 1978;
+
+// создаю функцию showGreeting
+function showGreeting(name) {
+    console.dir('Привет, ' + name + '!');
+};
+
+// вызов функции
+showGreeting(myName);
